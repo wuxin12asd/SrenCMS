ef8c7e2799325bd2f81794b6a38d567ba8da4a72
wuxin12asd <1925616965@QQ.COM>
2023-04-05 22:21:07 +0800

    初始化

---
+++ src/main/java/com/srencms/SrenCmsApplication.java
@@ -0,0 +1,13 @@
+package com.srencms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrenCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrenCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/srencms/SrenCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srencms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrenCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
